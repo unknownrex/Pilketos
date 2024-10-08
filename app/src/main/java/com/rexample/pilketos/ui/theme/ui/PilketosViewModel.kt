767daa16e045815f6fda6ff72a608110d35ab59b
Rex <reyvanfarrel@gmail.com>
2024-10-08 09:21:52 +0700

    Added Login logic

--- app/src/main/java/com/rexample/pilketos/ui/theme/ui/PilketosViewModel.kt
+++ app/src/main/java/com/rexample/pilketos/ui/theme/ui/PilketosViewModel.kt
@@ -1,11 +1,7 @@
 package com.rexample.pilketos.ui.theme.ui
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.rexample.pilketos.data.LocalCalonKetosData
-import com.rexample.pilketos.model.CalonKetos
+import com.rexample.pilketos.data.LocalAccountData
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
@@ -14,7 +10,19 @@ class PilketosViewModel : ViewModel() {
     private val _uiState = MutableStateFlow(PilketosUiState())
     val uiState: StateFlow<PilketosUiState> = _uiState
 
-    fun voteCalon(calondId: Int) {
+    fun Login(nisnInput: String, passwordInput: String) {
+        val account = LocalAccountData.defaultAccount
+        if(nisnInput == account.nisnSiswa && passwordInput == account.passwordAccount) {
+            _uiState.update {
+                it.copy(isUserLoggedIn = true, loginError = false)
+            }
+        } else {
+            _uiState.update {
+                it.copy(loginError = true)
+            }
+        }
+    }
+    fun VoteCalon(calondId: Int) {
         _uiState.update {
             it.copy(
                 votedCalon = calondId,
@@ -24,7 +32,7 @@ class PilketosViewModel : ViewModel() {
         }
     }
 
-    fun showVoteDialog() {
+    fun ShowVoteDialog() {
         _uiState.update {
             it.copy(
                 isShowingVoteDialog = true
@@ -32,7 +40,7 @@ class PilketosViewModel : ViewModel() {
         }
     }
 
-    fun hideVoteDialog() {
+    fun HideVoteDialog() {
         _uiState.update {
             it.copy(
                 isShowingVoteDialog = false
