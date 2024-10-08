e8a95e2a926d1d9468227afa758496cfd4b553fc
Rex <reyvanfarrel@gmail.com>
2024-10-08 09:30:51 +0700

    Removed unused import & connect to viewmodel

--- app/src/main/java/com/rexample/pilketos/ui/theme/ui/LoginScreen.kt
+++ app/src/main/java/com/rexample/pilketos/ui/theme/ui/LoginScreen.kt
@@ -14,9 +14,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,45 +27,64 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rexample.pilketos.R
-import com.rexample.pilketos.ui.theme.PILKETOSTheme
 import com.rexample.pilketos.ui.theme.poppins
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.rexample.pilketos.data.BottomNavItem
 
 @Composable
-fun LoginScreen() {
-    Scaffold { innerPadding ->
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    viewModel: PilketosViewModel = viewModel(),
+    navController: NavHostController
+) {
+    var nisnInput by remember { mutableStateOf("") }
+    var passwordInput by remember { mutableStateOf("") }
+    val state by viewModel.uiState.collectAsState()
+
+    if (state.isUserLoggedIn) {
+        LaunchedEffect(Unit) {
+            navController.navigate(BottomNavItem.Voting.route) {
+                popUpTo(navController.graph.startDestinationId) { inclusive = true }
+                launchSingleTop = true
+            }
+        }
+    }
+
+    Column(
+        modifier
+            .padding(30.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+
+    ) {
+        PilketosLogo(Modifier.padding(top = 90.dp, bottom = 90.dp))
         Column(
             Modifier
-                .padding(innerPadding)
-                .padding(30.dp)
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
-
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            PilketosLogo(Modifier.padding(top = 90.dp, bottom = 90.dp))
-            Column(
-                Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(
-                    text = "Login",
-                    style = MaterialTheme.typography.titleLarge
-                )
-                Spacer(Modifier.height(24.dp))
-                LoginField(onLoginClick = {})
-                Spacer(Modifier.height(24.dp))
-                Text(
-                    text = "Lupa password?",
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Medium,
-                    color = MaterialTheme.colorScheme.onSecondary
-                )
-
-            }
+            Text(
+                text = "Login",
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(Modifier.height(24.dp))
+            LoginField(
+                state = state,
+                onLoginClick = { nisnInput, passwordInput ->
+                    viewModel.Login(nisnInput, passwordInput)
+                }
+            )
+            Spacer(Modifier.height(24.dp))
+            Text(
+                text = "Lupa password?",
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colorScheme.onSecondary
+            )
 
         }
     }
@@ -73,39 +93,55 @@ fun LoginScreen() {
 
 @Composable
 fun LoginField(
-    onLoginClick: () -> Unit
+    state: PilketosUiState,
+    onLoginClick: (String, String) -> Unit
 ) {
-    var nisn by remember { mutableStateOf("") }
-    var password by remember { mutableStateOf("") }
+    var nisnInput by remember { mutableStateOf("") }
+    var passwordInput by remember { mutableStateOf("") }
+
     Column {
         OutlinedTextField(
-            value = nisn,
+            value = nisnInput,
             shape = RoundedCornerShape(8.dp),
-            onValueChange = {nisn = it},
-            label = {Text("NISN")},
+            onValueChange = {nisnInput = it},
+            label = {
+                Text(text = "NISN", color = MaterialTheme.colorScheme.secondaryContainer)
+            },
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
-            textStyle = MaterialTheme.typography.labelMedium
+            textStyle = MaterialTheme.typography.labelMedium,
+            isError = state.loginError
         )
 
         Spacer(modifier = Modifier.height(16.dp))
 
         OutlinedTextField(
-            value = password,
+            value = passwordInput,
             shape = RoundedCornerShape(8.dp),
-            onValueChange = { password = it },
-            label = { Text("Password") },
+            onValueChange = { passwordInput = it },
+            label = {
+                Text(text = "Password", color = MaterialTheme.colorScheme.secondaryContainer)
+            },
             singleLine = true,
             visualTransformation = PasswordVisualTransformation(),
             modifier = Modifier.fillMaxWidth(),
-            textStyle = MaterialTheme.typography.labelMedium
+            textStyle = MaterialTheme.typography.labelMedium,
+            isError = state.loginError
         )
+        if (state.loginError) {
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "NISN atau Password salah. Silahkan coba lagi",
+                color = MaterialTheme.colorScheme.error,
+                fontWeight = FontWeight.Medium,
+                fontSize = 14.sp
+            )
+        }
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // Tombol Login
         Button(
-            onClick = onLoginClick,
+            onClick = { onLoginClick(nisnInput, passwordInput)},
             shape = RoundedCornerShape(8.dp),
             modifier = Modifier
                 .fillMaxWidth()
@@ -133,21 +169,5 @@ fun PilketosLogo(modifier: Modifier = Modifier) {
             contentDescription = null,
             modifier = Modifier.width(250.dp)
         )
-    }
-}
-
-@Preview
-@Composable
-fun AppPreview() {
-    PILKETOSTheme {
-        LoginScreen()
-    }
-}
-
-@Preview
-@Composable
-fun LogoPreview() {
-    PILKETOSTheme {
-        PilketosLogo()
     }
 }
