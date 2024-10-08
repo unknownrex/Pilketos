28a63892894350c19bd39bc736d09d9920f69da1
Rex <reyvanfarrel@gmail.com>
2024-10-08 09:30:29 +0700

    Removed unused import

--- app/src/main/java/com/rexample/pilketos/MainActivity.kt
+++ app/src/main/java/com/rexample/pilketos/MainActivity.kt
@@ -3,24 +3,16 @@ package com.rexample.pilketos
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.rexample.pilketos.ui.theme.PILKETOSTheme
-import com.rexample.pilketos.ui.theme.ui.LoginScreen
+import com.rexample.pilketos.ui.theme.ui.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
+
             PILKETOSTheme {
-                LoginScreen()
+                MainScreen()
             }
         }
     }
